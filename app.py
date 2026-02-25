fd892f4f81eaf74d0179b7a020e9610c803290fc
sneh prajapti <22bmiit099@gmail.com>
2026-02-25 20:39:27 +0530

    Add missing imports for nltk and os in app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, jsonify
 from config import Config
 from sentiment_predictor import predictor
+import nltk
+import os
 
 app = Flask(__name__)
 app.config.from_object(Config)
